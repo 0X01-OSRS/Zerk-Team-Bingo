fe486c226217ab7b941f918a5ada1edb6b3c04e9
0X01 <charger_ciders_0t@icloud.com>
2026-01-08 17:10:45 -0800

    improve bingoItems json
    move the json to a separate file

---
+++ src/bingo_items.jsx
@@ -0,0 +1,361 @@
+export const bingoItems = [
+    {
+        index: 1,
+        image: "./tiles/tile1.png",
+        title: "Venomous Jeweler",
+        text: (
+            <ul>
+                <li>Araxyte fang</li>
+                <li>Onyx</li>
+                <li>Zenyte shard</li>
+            </ul>
+        ),
+    },
+    {
+        index: 2,
+        image: "./tiles/tile2.png",
+        title: "Catch These Hands",
+        text: (
+            <ul>
+                <li>Mokhaiotl cloth</li>
+                <li>Onyx</li>
+                <li>Zenyte shard</li>
+            </ul>
+        ),
+    },
+    {
+        index: 3,
+        image: "./tiles/tile3.png",
+        title: "Demonic Arsenal",
+        text: (
+            <ul>
+                <li>Any abyssal weapon</li>
+                <li>Tormented synapse</li>
+                <li>Eye of ayak</li>
+            </ul>
+        ),
+    },
+    {
+        index: 4,
+        image: "./tiles/tile4.png",
+        title: "Eternal Love",
+        text: (
+            <ul>
+                <li>Imbued heart</li>
+                <li className="or">OR</li>
+                <li>Eternal gem</li>
+            </ul>
+        ),
+    },
+    {
+        index: 5,
+        image: "./tiles/tile5.png",
+        title: "Triple Double",
+        text: (
+            <ul>
+                <li>Tonalztics of ralos</li>
+                <li>Dark bow</li>
+                <li>Fire element staff crown</li>
+                <li>Ice element staff crown</li>
+            </ul>
+        ),
+    },
+    {
+        index: 6,
+        image: "./tiles/tile6.png",
+        title: "Ranged Fortification",
+        text: (
+            <ul>
+                <li>Masori armor piece</li>
+                <li>Armadyl armor piece</li>
+            </ul>
+        ),
+    },
+    {
+        index: 7,
+        image: "./tiles/tile7.png",
+        title: "Melee Fortification",
+        text: (
+            <ul>
+                <li>Torva armor piece</li>
+                <li>Bandos armor piece (excluding Bandos boots)</li>
+            </ul>
+        ),
+    },
+    {
+        index: 8,
+        image: "./tiles/tile8.png",
+        title: "What Was That?",
+        text: (
+            <ul>
+                <li>Bandos boots</li>
+                <li>Black tourmaline core</li>
+                <li>Echo crystal</li>
+            </ul>
+        ),
+    },
+    {
+        index: 9,
+        image: "./tiles/tile9.png",
+        title: "The Lizard Sticker",
+        text: (
+            <ul>
+                <li>Zamorakian spear</li>
+                <li>Hydra's claw</li>
+            </ul>
+        ),
+    },
+    {
+        index: 10,
+        image: "./tiles/tile10.png",
+        title: "Theatre of Blood",
+        text: (
+            <ul>
+                <li>2 purples from Theatre of Blood</li>
+            </ul>
+        ),
+    },
+    {
+        index: 11,
+        image: "./tiles/tile11.png",
+        title: "Hellhound's Heels",
+        text: (
+            <ul>
+                <li>Pegasian crystal</li>
+                <li>Ranger boots</li>
+                <li className="or">OR</li>
+                <li>Eternal crystal</li>
+                <li>Infinity boots</li>
+                <li className="or">OR</li>
+                <li>Primordial crystal</li>
+                <li>Dragon boots</li>
+            </ul>
+        ),
+    },
+    {
+        index: 12,
+        image: "./tiles/tile12.png",
+        title: "Lord of the Bling",
+        text: (
+            <ul>
+                <li>DT2 Vestige</li>
+                <li>Matching DK ring</li>
+            </ul>
+        ),
+    },
+    {
+        index: 13,
+        image: "./tiles/tile13.png",
+        title: "Risk Assessment",
+        text: (
+            <ul>
+                <li>Craw's bow</li>
+                <li>Fangs of venenatis</li>
+                <li className="or">OR</li>
+                <li>Viggora's chainmace</li>
+                <li>Claws of callisto</li>
+                <li className="or">OR</li>
+                <li>Thammaron's sceptre</li>
+                <li>Skull of vet'ion</li>
+            </ul>
+        ),
+    },
+    {
+        index: 14,
+        image: "./tiles/tile14.png",
+        title: "Lesser of Two Evils",
+        text: (
+            <ul>
+                <li>Nightmare unique</li>
+                <li className="or">OR</li>
+                <li>Yama unique</li>
+            </ul>
+        ),
+    },
+    {
+        index: 15,
+        image: "./tiles/tile15.png",
+        title: "Chambers of Xeric",
+        text: (
+            <ul>
+                <li>2 purples from Chambers of Xeric</li>
+            </ul>
+        ),
+    },
+    {
+        index: 16,
+        image: "./tiles/tile16.png",
+        title: "Toxic Relationship",
+        text: (
+            <ul>
+                <li>Staff of the dead</li>
+                <li>Magic fang</li>
+            </ul>
+        ),
+    },
+    {
+        index: 17,
+        image: "./tiles/tile17.png",
+        title: "Who Sharded?",
+        text: (
+            <ul>
+                <li>Any Godsword hilt</li>
+                <li>3 unique Godsword shards</li>
+            </ul>
+        ),
+    },
+    {
+        index: 18,
+        image: "./tiles/tile18.png",
+        title: "Plunderin' Booty",
+        text: (
+            <ul>
+                <li>Tiny pearl</li>
+                <li>Ray barbs</li>
+                <li>Swift albatross feather</li>
+                <li>Echo pearl</li>
+                <li>Narwhal horn</li>
+                <li>Dragon metal sheet</li>
+                <li>Dragon nails</li>
+            </ul>
+        ),
+    },
+    {
+        index: 19,
+        image: "./tiles/tile19.png",
+        title: "Elemental Elegance",
+        text: (
+            <ul>
+                <li>Tome of fire</li>
+                <li>Tome of water</li>
+                <li>Tome of earth</li>
+            </ul>
+        ),
+    },
+    {
+        index: 20,
+        image: "./tiles/tile20.png",
+        title: "Tombs of Amascut",
+        text: (
+            <ul>
+                <li>2 purples from Tombs of Amascut</li>
+            </ul>
+        ),
+    },
+    {
+        index: 21,
+        image: "./tiles/tile21.png",
+        title: "Weaponized Moontism",
+        text: (
+            <ul>
+                <li>Dual macuahuitl</li>
+                <li>Eclipse atlatl</li>
+                <li>Blue moon spear</li>
+            </ul>
+        ),
+    },
+    {
+        index: 22,
+        image: "./tiles/tile22.png",
+        title: "Phantom Dancer",
+        text: (
+            <ul>
+                <li>3 Venator shards</li>
+            </ul>
+        ),
+    },
+    {
+        index: 23,
+        image: "./tiles/tile23.png",
+        title: "Gryphon Tamer",
+        text: (
+            <ul>
+                <li>Belle's folly</li>
+            </ul>
+        ),
+    },
+    {
+        index: 24,
+        image: "./tiles/tile24.png",
+        title: "Skills Master",
+        text: (
+            <ul>
+                <li style={{ color: "white" }}>Choose 3:</li>
+                <li>Fletching knife</li>
+                <li>Tackle box</li>
+                <li>Fish barrel</li>
+                <li>Bruma torch</li>
+                <li>Strange old lockpick</li>
+                <li>Imcando hammer</li>
+            </ul>
+        ),
+    },
+    {
+        index: 25,
+        image: "./tiles/tile25.png",
+        title: "Bob Ross",
+        text: (
+            <ul>
+                <li>3 tradeable boat paints</li>
+                <li>(excluding Merchant's paint,</li>
+                <li>dupes of the same paint <span style='color:white'>are allowed</span>)</li>
+            </ul>
+        ),
+    },
+    {
+        index: 26,
+        image: "./tiles/tile26.png",
+        title: "Brothers Brawl",
+        text: (
+            <ul>
+                <li>Full set of Barrows armor (any helm/chest/legs)</li>
+            </ul>
+        ),
+    },
+    {
+        index: 27,
+        image: "./tiles/tile27.png",
+        title: "King of the Rats",
+        text: (
+            <ul>
+                <li>Brine sabre</li>
+                <li>3 Scurrius' spines</li>
+            </ul>
+        ),
+    },
+    {
+        index: 28,
+        image: "./tiles/tile28.png",
+        title: "Freaky Forester",
+        text: (
+            <ul>
+                <li>Golden pheasant egg</li>
+                <li>Fox whistle</li>
+            </ul>
+        ),
+    },
+    {
+        index: 29,
+        image: "./tiles/tile29.png",
+        title: "Medieval Meleer",
+        text: (
+            <ul>
+                <li>Sulphur blades</li>
+                <li>Glacial temotli</li>
+                <li>Earthbound tecpatl</li>
+            </ul>
+        ),
+    },
+    {
+        index: 30,
+        image: "./tiles/tile30.png",
+        title: "FeelsBadMan",
+        text: (
+            <ul>
+                <li>Champion scroll</li>
+                <li>Curved bone</li>
+                <li>Long bone</li>
+            </ul>
+        ),
+    },
+];
